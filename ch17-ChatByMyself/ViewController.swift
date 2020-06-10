2184252a4e35dca624977bbca1e81031a7542231
박혜연 <hyeyeonpark7183@gmail.com>
2020-06-10 13:41:03 +0900

    Initial Commit

---
+++ ch17-ChatByMyself/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ch17-ChatByMyself
+//
+//  Created by admin on 2020/06/10.
+//  Copyright © 2020 admin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
